caaae656597b138d823f71a12c330af934159de4
zhaochy <zhaochy_2015@hotmail.com>
2025-04-28 02:22:14 +0000

    update install.sh
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

--- install.sh
+++ install.sh
@@ -15,16 +15,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Step2: setting up symlinks"
-lnif "$CURRENT_DIR/" "$HOME/.config/nvim"
-
-echo "Step3: install Plugins with Plug"
-nvim -u $HOME/.config/nvim/init.vim +PlugInstall! +PlugClean! +qall
-
-echo "Step4: build YCM Core"
-
-cd $CURRENT_DIR/plugged/youcompleteme/
-git submodule update --init --recursive
-python install.py --ts-completer --go-completer
+lnif "$CURRENT_DIR/nvim" "$HOME/.config/nvim"
 
 echo "done"
 
